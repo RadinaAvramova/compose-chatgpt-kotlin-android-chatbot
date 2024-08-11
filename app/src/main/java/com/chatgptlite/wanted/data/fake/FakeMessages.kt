ce6572a0a7cc5d52535fa04a9eaab7a2dc00bcc8
Radina Avramova <99686592+RadinaAvramova@users.noreply.github.com>
2024-08-11 17:23:42 +0300

    Update FakeMessages.kt

--- app/src/main/java/com/chatgptlite/wanted/data/fake/FakeMessages.kt
+++ app/src/main/java/com/chatgptlite/wanted/data/fake/FakeMessages.kt
@@ -5,13 +5,13 @@ import java.util.*
 
 val fakeMessages: List<MessageModel> = listOf(
     MessageModel(
-        question = "Who is lambiengcode?",
-        answer = "I'm Kai (lambiengcode), currently working as the Technical Leader at Askany and Waodate. Computador Also, I'm a freelancer. If you have a need for a mobile application or website, contact me by email: lambiengcode@gmail.com",
+        question = "",
+        answer = "",
         createdAt = Date()
     ),
     MessageModel(
-        question = "Who is lambiengcode?",
-        answer = "I'm Kai (lambiengcode), currently working as the Technical Leader at Askany and Waodate. Computador Also, I'm a freelancer. If you have a need for a mobile application or website, contact me by email: lambiengcode@gmail.com",
+        question = "",
+        answer = ",
         createdAt = Date()
     ),
 
